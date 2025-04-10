8661d4467e5daad629db97aa9b30f0452a038d83
Gabriel <gabrielfg12345@gmail.com>
2025-04-10 17:56:32 -0300

    Atualização calculadora dia 10 ---------------------- ---------------------- Ordem-Servico: SGPS-#9999999

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
 
-        float numero1;
-        float numero2;
+        float primeiro_numero;
+        float segundo_numero;
         float resultado;
         int operacao;
+        // 0 = soma, 1 = subtração, 2 = multiplicação, 3 = divisão
 
-        operacao = 3; // 0 = soma, 1 = subtração, 2 = multiplicação, 3 = divisão
-        numero1 = 0f;
-        numero2 = 0f;
+        System.out.println("Selecione a operação desejada:");
+        System.out.println("0 - Soma");
+        System.out.println("1 - Subtração");
+        System.out.println("2 - Multiplicação");
+        System.out.println("3 - Divisão");
+        operacao = myObj.nextInt();
+        System.out.println("Perfeito! Agora selecione seu primeiro número:");
+        primeiro_numero = myObj.nextFloat();
+        System.out.println("Selecione seu segundo número:");
+        segundo_numero = myObj.nextFloat();
+        System.out.println("Calculando...");
+
+
+
 
         switch (operacao) {
             case 0: //soma
-                resultado = (numero1) + (numero2);
-                System.out.println("SOMA!");
-                System.out.println((numero1) + " + " + (numero2) + " = ");
-                System.out.println("Resultado: " + resultado);
+                resultado = (primeiro_numero) + (segundo_numero);
+                System.out.println((primeiro_numero) + " + " + (segundo_numero) + " = " + resultado);
                 break;
 
             case 1: //subtração
-                resultado = (numero1) - (numero2);
-                System.out.println("SUBTRAÇÃO!");
-                System.out.println((numero1) + " - " + (numero2) + " = ");
-                System.out.println("Resultado: " + resultado);
+                resultado = (primeiro_numero) - (segundo_numero);
+                System.out.println((primeiro_numero) + " - " + (segundo_numero) + " = " + resultado);
                 break;
 
             case 2: //multiplicação
-                resultado = (numero1) * (numero2);
-                System.out.println("MULTIPLICAÇÃO!");
-                System.out.println((numero1) + " * " + (numero2) + " = ");
-                System.out.println("Resultado: " + resultado);
+                resultado = (primeiro_numero) * (segundo_numero);
+                System.out.println((primeiro_numero) + " * " + (segundo_numero) + " = " + resultado);
                 break;
 
             case 3: //divisão
-                if ((numero2) >= 1 || (numero2) <= -1) {
-                    resultado = (numero1) / (numero2);
-                    System.out.println("DIVISÃO!");
-                    System.out.println((numero1) + " ÷ " + (numero2) + " = ");
-                    System.out.println("Resultado: " + resultado);
-                    System.out.println("Resto: " + (numero1) % (numero2));
+                if ((segundo_numero) >= 1 || (segundo_numero) <= -1) {
+                    resultado = (primeiro_numero) / (segundo_numero);
+                    System.out.println((primeiro_numero) + " ÷ " + (segundo_numero) + " = " + resultado);
+                    System.out.println("Resto: " + (primeiro_numero) % (segundo_numero));
                 }
                 else {
                     System.out.println("Divisão não permitida.");
