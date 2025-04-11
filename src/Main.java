5db1e1603d7ccd154e41612fdcb6b3085e1f9467
Gabriel <gabrielfg12345@gmail.com>
2025-04-11 15:53:57 -0300

    Loop adicionado ---------------------- Adição de loop e correção de bugs ---------------------- Ordem-Servico: SGPS-#9999999

--- src/Main.java
+++ src/Main.java
@@ -4,51 +4,70 @@ public class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
 
-        float primeiro_numero;
-        float segundo_numero;
+        float PrimeiroNumero;
+        float SegundoNumero;
         float resultado;
         int operacao;
-        // 0 = soma, 1 = subtração, 2 = multiplicação, 3 = divisão
 
+        // 0 = soma, 1 = subtração, 2 = multiplicação, 3 = divisão
+        System.out.println("Olá! Bem-vindo à calculadora!");
         System.out.println("Selecione a operação desejada:");
         System.out.println("0 - Soma");
         System.out.println("1 - Subtração");
         System.out.println("2 - Multiplicação");
         System.out.println("3 - Divisão");
+
         operacao = myObj.nextInt();
+        if (operacao < 0 || operacao > 3) {
+            System.out.println("Operação inválida.");
+            main(args);
+        }
         System.out.println("Perfeito! Agora selecione seu primeiro número:");
-        primeiro_numero = myObj.nextFloat();
+        PrimeiroNumero = myObj.nextFloat();
         System.out.println("Selecione seu segundo número:");
-        segundo_numero = myObj.nextFloat();
+        SegundoNumero = myObj.nextFloat();
         System.out.println("Calculando...");
 
         switch (operacao) {
             case 0: //soma
-                resultado = (primeiro_numero) + (segundo_numero);
-                System.out.println((primeiro_numero) + " + " + (segundo_numero) + " = " + resultado);
+                resultado = (PrimeiroNumero) + (SegundoNumero);
+                System.out.println((PrimeiroNumero) + " + " + (SegundoNumero) + " = " + resultado);
                 break;
 
             case 1: //subtração
-                resultado = (primeiro_numero) - (segundo_numero);
-                System.out.println((primeiro_numero) + " - " + (segundo_numero) + " = " + resultado);
+                resultado = (PrimeiroNumero) - (SegundoNumero);
+                System.out.println((PrimeiroNumero) + " - " + (SegundoNumero) + " = " + resultado);
                 break;
 
             case 2: //multiplicação
-                resultado = (primeiro_numero) * (segundo_numero);
-                System.out.println((primeiro_numero) + " * " + (segundo_numero) + " = " + resultado);
+                resultado = (PrimeiroNumero) * (SegundoNumero);
+                System.out.println((PrimeiroNumero) + " * " + (SegundoNumero) + " = " + resultado);
                 break;
 
             case 3: //divisão
-                if ((segundo_numero) >= 1 || (segundo_numero) <= -1) {
-                    resultado = (primeiro_numero) / (segundo_numero);
-                    System.out.println((primeiro_numero) + " ÷ " + (segundo_numero) + " = " + resultado);
-                    System.out.println("Resto: " + (primeiro_numero) % (segundo_numero));
+                if ((SegundoNumero) >= 1 || (SegundoNumero) <= -1) {
+                    resultado = (PrimeiroNumero) / (SegundoNumero);
+                    System.out.println((PrimeiroNumero) + " ÷ " + (SegundoNumero) + " = " + resultado);
+                    System.out.println("Resto: " + (PrimeiroNumero) % (SegundoNumero));
                 } else {
                     System.out.println("Divisão não permitida.");
-
                 }
                 break;
+        }
 
+        System.out.println("Deseja realizar outra operação? (s/n)");
+        Scanner myAns = new Scanner(System.in);
+        String resposta = myAns.next();
+        if (resposta.equalsIgnoreCase("s")) {
+            main(args);
+        } else if (resposta.equalsIgnoreCase("n")) {
+            System.out.println("Obrigado por usar a calculadora!");
+        } else {
+            System.out.println("Opção inválida.");
         }
     }
 }
+
+
+
+
