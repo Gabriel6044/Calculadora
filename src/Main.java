874065507da5947081380e1f75d6480da12c9588
Gabriel <gabrielfg12345@gmail.com>
2025-04-11 18:00:01 -0300

    Iniciando continuação de conta ---------------------- ---------------------- Ordem-Servico: SGPS-#9999999

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,13 @@ public class Main {
 
         float PrimeiroNumero;
         float SegundoNumero;
+        float NumeroSeguinte;
         float resultado;
         int operacao;
         boolean keepRunning = true;
 
-        while(keepRunning) {
-
-            // 0 = soma, 1 = subtração, 2 = multiplicação, 3 = divisão
-            System.out.println("Olá! Bem-vindo à calculadora!");
-            System.out.println("Selecione a operação desejada:");
-            System.out.println("0 - Soma");
-            System.out.println("1 - Subtração");
-            System.out.println("2 - Multiplicação");
-            System.out.println("3 - Divisão");
+        while (keepRunning) {
+            explicacao();
 
             operacao = getOperacao(myObj);
             System.out.println("Perfeito! Agora selecione seu primeiro número:");
@@ -29,39 +23,77 @@ public class Main {
 
             switch (operacao) {
                 case 0: //soma
-                    resultado = (PrimeiroNumero) + (SegundoNumero);
-                    System.out.println((PrimeiroNumero) + " + " + (SegundoNumero) + " = " + resultado);
+                    soma(PrimeiroNumero, SegundoNumero);
                     break;
 
                 case 1: //subtração
-                    resultado = (PrimeiroNumero) - (SegundoNumero);
-                    System.out.println((PrimeiroNumero) + " - " + (SegundoNumero) + " = " + resultado);
+                    subtracao(PrimeiroNumero, SegundoNumero);
                     break;
 
                 case 2: //multiplicação
-                    resultado = (PrimeiroNumero) * (SegundoNumero);
-                    System.out.println((PrimeiroNumero) + " * " + (SegundoNumero) + " = " + resultado);
+                    multiplicacao(PrimeiroNumero, SegundoNumero);
                     break;
 
                 case 3: //divisão
-                    if ((SegundoNumero) >= 1 || (SegundoNumero) <= -1) {
-                        resultado = (PrimeiroNumero) / (SegundoNumero);
-                        System.out.println((PrimeiroNumero) + " ÷ " + (SegundoNumero) + " = " + resultado);
-                        System.out.println("Resto: " + (PrimeiroNumero) % (SegundoNumero));
-                    } else {
-                        System.out.println("Divisão impossível.");
-                    }
+                    divisao(SegundoNumero, PrimeiroNumero);
                     break;
             }
 
             System.out.println("Deseja realizar outra operação? (s/n)");
             Scanner myAns = new Scanner(System.in);
             String resposta = getResposta(myAns);
-            if (resposta.equalsIgnoreCase("n")){
+            if (resposta.equalsIgnoreCase("n")) {
                 System.out.println("Obrigado por usar a calculadora");
                 keepRunning = false;
             }
+
+            System.out.println("Manter resultados anteriores? (s/n)");
+            Scanner myAns2 = new Scanner(System.in);
+            String resposta2 = getResposta(myAns2);
+            if (resposta2.equalsIgnoreCase("n")) {
+                main(args); // Reinicia o programa
+            }
+            System.out.println("Perfeito! Vamos continuar com os resultados anteriores.");
+            PrimeiroNumero = resultado;
         }
+    }
+
+    private static void divisao(float SegundoNumero, float PrimeiroNumero) {
+        float resultado;
+        if (SegundoNumero >= 1 || SegundoNumero <= -1) {
+            resultado = PrimeiroNumero / SegundoNumero;
+            System.out.println(PrimeiroNumero + " ÷ " + SegundoNumero + " = " + resultado);
+            System.out.println("Resto: " + PrimeiroNumero % SegundoNumero);
+        } else {
+            System.out.println("Divisão impossível.");
+        }
+    }
+
+    private static void multiplicacao(float PrimeiroNumero, float SegundoNumero) {
+        float resultado;
+        resultado = PrimeiroNumero * SegundoNumero;
+        System.out.println(PrimeiroNumero + " * " + SegundoNumero + " = " + resultado);
+    }
+
+    private static void subtracao(float PrimeiroNumero, float SegundoNumero) {
+        float resultado;
+        resultado = PrimeiroNumero - SegundoNumero;
+        System.out.println(PrimeiroNumero + " - " + SegundoNumero + " = " + resultado);
+    }
+
+    private static void soma(float PrimeiroNumero, float SegundoNumero) {
+        float resultado;
+        resultado = PrimeiroNumero + SegundoNumero;
+        System.out.println(PrimeiroNumero + " + " + SegundoNumero + " = " + resultado);
+    }
+
+    private static void explicacao() {
+        System.out.println("Olá! Bem-vindo à calculadora!");
+        System.out.println("Selecione a operação desejada:");
+        System.out.println("0 - Soma");
+        System.out.println("1 - Subtração");
+        System.out.println("2 - Multiplicação");
+        System.out.println("3 - Divisão");
     }
 
     private static String getResposta(Scanner myAns) {
@@ -76,28 +108,26 @@ public class Main {
 
     private static float getNumero(Scanner myObj) {
         try {
-            float numero;
-            numero = myObj.nextFloat();
-            return numero;
-        }catch (Exception e){
+            return myObj.nextFloat();
+        } catch (Exception e) {
             System.out.println("Número inválido. Tente novamente.");
             myObj.next(); // Limpa o buffer
             return getNumero(myObj);
         }
     }
 
-    private static int getOperacao(Scanner myObj) {
+    private static int getOperacao(Scanner objeto) {
         try {
             int operacao;
-            operacao = myObj.nextInt();
+            operacao = objeto.nextInt();
             if (operacao < 0 || operacao > 3) {
                 throw new Exception("Operação inválida");
             }
             return operacao;
-        }catch (Exception e) {
-            System.out.println("Operação inválido. Tente novamente.");
-            myObj.next(); // Limpa o buffer
-            return getOperacao(myObj);
+        } catch (Exception e) {
+            System.out.println("Operação inválida. Tente novamente.");
+            objeto.next(); // Limpa o buffer
+            return getOperacao(objeto);
         }
     }
 
